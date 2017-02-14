c51f3f2cb6ed936793047014deb70603492221cd
Javier Gamarra <nhpatt@gmail.com>
2017-02-14 06:56:41 +0100

    try live data binding

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,6 +1,5 @@
-import { Component } from '@angular/core';
-
-import { NavController } from 'ionic-angular';
+import {Component} from "@angular/core";
+import {NavController} from "ionic-angular";
 
 @Component({
   selector: 'page-home',
@@ -8,10 +7,10 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  talk = 'Hi!'
+  talk = 'Hi!';
 
   constructor(public navCtrl: NavController) {
-
+    setTimeout(() => this.talk = 'Hello!', 2000)
   }
 
 }
