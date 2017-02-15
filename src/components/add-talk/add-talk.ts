d4627ad8552b29c1d9a8dccfd19a16a848c2f0d6
Javier Gamarra <nhpatt@gmail.com>
2017-02-15 12:25:47 +0100

    using form controls

--- src/components/add-talk/add-talk.ts
+++ src/components/add-talk/add-talk.ts
@@ -1,6 +1,7 @@
 import {Component} from "@angular/core";
 import {TalkService} from "../../providers/talkService";
 import {Talk} from "../../pages/Talk";
+import {FormControl} from "@angular/forms";
 
 @Component({
   selector: 'add-talk',
@@ -9,12 +10,16 @@ import {Talk} from "../../pages/Talk";
 })
 export class AddTalkComponent {
 
+  name: FormControl;
+
   constructor(public talkService: TalkService) {
   }
 
   addTalk(form) {
 
-    console.log(form);
+    this.name = form.controls.name;
+    this.name.hasError('required');
+    console.log(form, this.name);
 
     // let talk = new Talk(name, speaker, "workshop");
     // this.talkService.addTalk(talk).subscribe(x => console.log(x));
